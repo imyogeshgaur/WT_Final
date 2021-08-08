e33c469d9eb7d5f3960ba8177e4d1d4603a91b1c
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-08 17:24:42 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaNumber/bouncyNumber.java
@@ -0,0 +1,44 @@
+package JavaNumber;
+
+import java.util.Scanner;
+
+public class bouncyNumber {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter any number you want to check: ");
+        int inputNumber = scan.nextInt();
+        scan.close();
+        if (isIncreasing(inputNumber) || isDecreasing(inputNumber) || inputNumber < 101)
+            System.out.println(inputNumber + " not a bouncy number.");
+        else
+            System.out.println(inputNumber + " is a bouncy number.");
+    }
+
+    public static boolean isIncreasing(int inputNumber) {
+        String str = Integer.toString(inputNumber);
+        char digit;
+        boolean flag = true;
+        for (int i = 0; i < str.length() - 1; i++) {
+            digit = str.charAt(i);
+            if (digit > str.charAt(i + 1)) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    public static boolean isDecreasing(int inputNumber) {
+        String str = Integer.toString(inputNumber);
+        char digit;
+        boolean flag = true;
+        for (int i = 0; i < str.length() - 1; i++) {
+            digit = str.charAt(i);
+            if (digit < str.charAt(i + 1)) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
