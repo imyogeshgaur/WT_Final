5bf713ef082506c270b455e1d6070810b22414bf
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-08 17:20:55 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaNumber/emripNumber.java
@@ -0,0 +1,38 @@
+package JavaNumber;
+
+import java.util.Scanner;
+
+public class emripNumber {
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
+    public static boolean isEmirp(int n) {
+        if (isPrime(n) == false)
+            return false;
+        int reverse = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+        return isPrime(reverse);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to check: ");
+        int n = sc.nextInt();
+        sc.close();
+        if (isEmirp(n) == true)
+            System.out.println("Yes, the given number is an emirp number.");
+        else
+            System.out.println("No, the given number is not an emirp number.");
+    }
+}
