000ae8a71bb273a280cdc0afcd43588cc90df877
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-08 17:16:56 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaBasic/objectCreation.java
@@ -0,0 +1,12 @@
+package JavaBasic;
+public class objectCreation {
+    void show() {
+        System.out.println("Welcome to javaTpoint");
+    }
+
+    public static void main(String[] args) {
+
+        objectCreation obj = new objectCreation();
+        obj.show();
+    }
+}
