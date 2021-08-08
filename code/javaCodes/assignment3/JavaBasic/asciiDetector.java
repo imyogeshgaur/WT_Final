65a999bd3755d60e1c42b7ef3765532212cd8953
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-08 17:15:42 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaBasic/asciiDetector.java
@@ -0,0 +1,12 @@
+package JavaBasic;
+public class asciiDetector {
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
