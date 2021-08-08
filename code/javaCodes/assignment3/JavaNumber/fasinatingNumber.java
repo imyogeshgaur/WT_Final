e8d962fa949f4015305842fafa8eee5539e43548
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-08 17:24:58 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaNumber/fasinatingNumber.java
@@ -0,0 +1,34 @@
+package JavaNumber;
+
+import java.util.Scanner;
+
+public class fasinatingNumber {
+    public static void main(String args[]) {
+        int num, n2, n3;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any Number: ");
+        num = sc.nextInt();
+        sc.close();
+        n2 = num * 2;
+        n3 = num * 3;
+
+        String concatstr = num + "" + n2 + n3;
+        boolean found = true;
+        for (char c = '1'; c <= '9'; c++) {
+            int count = 0;
+            for (int i = 0; i < concatstr.length(); i++) {
+                char ch = concatstr.charAt(i);
+                if (ch == c)
+                    count++;
+            }
+            if (count > 1 || count == 0) {
+                found = false;
+                break;
+            }
+        }
+        if (found)
+            System.out.println(num + " is a fascinating number.");
+        else
+            System.out.println(num + " is not a fascinating number.");
+    }
+}
