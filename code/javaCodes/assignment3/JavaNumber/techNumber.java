94c3734770848625f92975b49aa3b5e71971b685
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-08 17:25:33 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaNumber/techNumber.java
@@ -0,0 +1,31 @@
+package JavaNumber;
+
+import java.util.Scanner;
+
+public class techNumber {
+    public static void main(String args[]) {
+        int n, num, firstHalf, secondHalf, digits = 0, squareOfSum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to check: ");
+        n = sc.nextInt();
+        sc.close();
+        num = n;
+        while (num > 0) {
+            digits++;
+            num = num / 10;
+        }
+        if (digits % 2 == 0) {
+            num = n;
+            firstHalf = num % (int) Math.pow(10, digits / 2);
+            secondHalf = num / (int) Math.pow(10, digits / 2);
+            squareOfSum = (firstHalf + secondHalf) * (firstHalf + secondHalf);
+            if (n == squareOfSum) {
+                System.out.println(n + " is a tech number.");
+            } else {
+                System.out.println(n + " is not a tech number.");
+            }
+        } else {
+            System.out.println(n + " is not a tech number.");
+        }
+    }
+}
