f6d60f2b9d5edce25926665575257c1153db04ee
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-08 17:15:27 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaBasic/randomNumber.java
@@ -0,0 +1,10 @@
+package JavaBasic;
+public class randomNumber {
+    public static void main(String args[]) {
+
+        System.out.println("1st Random Number: " + Math.random());
+        System.out.println("2nd Random Number: " + Math.random());
+        System.out.println("3rd Random Number: " + Math.random());
+        System.out.println("4th Random Number: " + Math.random());
+    }
+}
