9e02f991a94662bc9fb97ed67993fd58104774e9
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-09 16:09:31 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaArray/printElement.java
@@ -0,0 +1,11 @@
+package JavaArray;
+
+public class printElement {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        for (int element : array) {
+            System.out.println(element);
+        }
+    }
+}
