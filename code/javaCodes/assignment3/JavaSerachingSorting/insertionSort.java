9010a332689cee17ab9e4beae20c9369fd061471
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-09 17:47:56 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaSerachingSorting/insertionSort.java
@@ -0,0 +1,29 @@
+package JavaSerachingSorting;
+
+import java.util.Arrays;
+
+public class insertionSort {
+    void InsertionSort(int array[]) {
+        int size = array.length;
+
+        for (int step = 1; step < size; step++) {
+            int key = array[step];
+            int j = step - 1;
+
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] data = { 9, 5, 1, 4, 3 };
+        insertionSort is = new insertionSort();
+        is.InsertionSort(data);
+        System.out.println("Sorted Array in Ascending Order: ");
+        System.out.println(Arrays.toString(data));
+    }
+}
