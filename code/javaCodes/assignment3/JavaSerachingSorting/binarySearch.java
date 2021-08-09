ba09b77368abb945ff95b781d5b0ada31333d4dc
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-09 17:46:50 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaSerachingSorting/binarySearch.java
@@ -0,0 +1,33 @@
+package JavaSerachingSorting;
+
+public class binarySearch {
+    int BinarySearch(int array[], int x, int low, int high) {
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (array[mid] == x)
+                return mid;
+
+            if (array[mid] < x)
+                low = mid + 1;
+
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        binarySearch ob = new binarySearch();
+        int array[] = { 3, 4, 5, 6, 7, 8, 9 };
+        int n = array.length;
+        int x = 4;
+        int result = ob.BinarySearch(array, x, 0, n - 1);
+        if (result == -1)
+            System.out.println("Not found");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
