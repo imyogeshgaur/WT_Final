c9be6c5eb8991c17517a879b9d21363e3a7c5eb8
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-08-10 07:45:33 +0530

    assignment 3 done

---
+++ code/javaCodes/assignment3/JavaCircularList/insertAtMid.java
@@ -0,0 +1,65 @@
+package JavaCircularList;
+
+public class insertAtMid {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    static void insertAtMiddle(int x) {
+        if (head == null)
+            head = new Node(x);
+        else {
+            Node newNode = new Node(x);
+
+            Node ptr = head;
+            int len = 0;
+
+            while (ptr != null) {
+                len++;
+                ptr = ptr.next;
+            }
+
+            int count = ((len % 2) == 0) ? (len / 2) : (len + 1) / 2;
+            ptr = head;
+
+            while (count-- > 1)
+                ptr = ptr.next;
+
+            newNode.next = ptr.next;
+            ptr.next = newNode;
+        }
+    }
+
+    static void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        head = null;
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(5);
+
+        System.out.println("Linked list before " + "insertion: ");
+        display();
+
+        int x = 3;
+        insertAtMiddle(x);
+
+        System.out.println("\nLinked list after" + " insertion: ");
+        display();
+    }
+}
